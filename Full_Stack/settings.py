9744af083eac4d95ae5f433e55359dfe696f463c
zhoomartq <zhomart.osmonaliev@gmail.com>
2021-06-22 16:01:18 +0600

    changed swagger

--- Full_Stack/settings.py
+++ Full_Stack/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'corsheaders',
     'django_filters',
 
-    'user'
+    'user',
+    'products',
 ]
 
 MIDDLEWARE = [
--- Full_Stack/urls.py
+++ Full_Stack/urls.py
@@ -20,12 +20,13 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/v1/docs/', schema_view.with_ui()),
     path('api/v1/products/', include('products.urls')),
+    path('api/v1/accounts/', include('user.urls'),),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
