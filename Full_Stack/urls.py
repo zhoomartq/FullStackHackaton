bec1a9b0dfa1a39314ad71c9d807ec179baafb05
Shabdan <a.shabdan@gmail.com>
2021-06-23 16:03:18 +0600

    like

--- Full_Stack/urls.py
+++ Full_Stack/urls.py
@@ -27,9 +27,9 @@ router.register('comment', CommentViewSet)
 
 
 urlpatterns = [
-    # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    # path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/v1/docs/', schema_view.with_ui()),
     path('api/v1/products/', include('products.urls')),
--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from products.models import Product, Comment, Favorite
+from products.models import Product, Comment, Favorite, Like
 
 admin.site.register(Product)
 admin.site.register(Comment)
 admin.site.register(Favorite)
+admin.site.register(Like)
